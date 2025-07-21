96e2bc89faa6cb098005936b04a67a8edcce7096
Marcelo Ribeiro Barbosa <marceloribeirobarbosa92@gmail.com>
2025-07-21 17:52:19 -0300

    refactor: renomear variável number para phone_number para evitar duplicidade

--- src/schemas/phoneSchema.ts
+++ src/schemas/phoneSchema.ts
@@ -2,7 +2,7 @@ import joi from "joi";
 import { PhonesData } from "protocols/protocolTypes";
 
 const phoneSchema = joi.object<PhonesData>({
-    number: joi.string().length(11).pattern(/^\d+$/).required(),
+    phone_number: joi.string().length(11).pattern(/^\d+$/).required(),
     carrier_id: joi.number().integer().required(),
     user_id: joi.number().integer().required(),
     description: joi.string().required()
