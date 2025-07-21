640eb126c64364e7d8f6e9e099f0514a1c0c27eb
Marcelo Ribeiro Barbosa <marceloribeirobarbosa92@gmail.com>
2025-07-21 18:35:12 -0300

    fix: erro de valores

--- src/repositories/phones-repository.ts
+++ src/repositories/phones-repository.ts
@@ -1,12 +1,24 @@
 import connection from "config/database";
-import { Request, Response } from "express";
-import { PhonesData } from "protocols/protocolTypes";
+import { NewPhoneData, PhonesData } from "protocols/protocolTypes";
 
 
 async function insertNewPhone(phoneData:PhonesData) {
     const {phone_number, carrier_id, user_id, description} = phoneData;
-    const result = await connection.query(`
+    const result = await connection.query<NewPhoneData>(`
         INSERT INTO phones (phone_number, carrier_id, user_id, description)
         VALUES ($1, $2, $3,$4)
+        RETURNING *
         `,[phone_number, carrier_id, user_id, description])
+
+    return result.rows[0];
+    
+
+    
 }
+
+
+const phoneRepositoy = {
+    insertNewPhone
+}
+
+export default phoneRepositoy;
