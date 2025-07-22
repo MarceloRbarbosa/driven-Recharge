e03b7fff279935f8c9b2467488ad97947a4dfffb
Marcelo Ribeiro Barbosa <marceloribeirobarbosa92@gmail.com>
2025-07-22 10:30:40 -0300

    feat: adicionar repositorios (create, findUserById, findUserByDocument) da tabela users

---
+++ src/repositories/users-repository.ts
@@ -0,0 +1,41 @@
+import connection from "config/database";
+import { CreateUser, User } from "protocols/protocolTypes";
+
+async function createNewUser(userData:CreateUser) {
+    const { document, name } = userData;
+    const result = await connection.query<User>(`
+        INSERT INTO users (document, name)
+        VALUES($1, $2)
+        RETURNING *
+        `, [ document, name]);
+        return result.rows[0];
+}
+
+async function findAllUsers() {
+    const users = await connection.query<User>(`SELECT * FROM users`)
+    return users.rows
+}
+
+async function findUserByDocument(document:String) {
+    const user = await connection.query<User>(`
+        SELECT * FROM users WHERE document = $1
+        `, [document])
+        return user.rows[0];
+}
+
+async function findUserById(id:Number) {
+    const user = await connection.query<User>(`
+        SELECT * FROM users WHERE id = $1
+        `,[id])
+        return user.rows[0]
+}
+
+
+const repository = {
+    createNewUser,
+    findAllUsers,
+    findUserByDocument,
+    findUserById
+}
+
+export default repository;
