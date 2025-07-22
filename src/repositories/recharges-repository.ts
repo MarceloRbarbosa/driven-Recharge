846a01b34e4e5cbbca668ec999fb9c7e18550665
Marcelo Ribeiro Barbosa <marceloribeirobarbosa92@gmail.com>
2025-07-22 10:54:39 -0300

    feat: adicionar funções do repositório 'recharges'

---
+++ src/repositories/recharges-repository.ts
@@ -0,0 +1,32 @@
+import connection from "config/database";
+import { CreaterRecharge, Recharge } from "protocols/protocolTypes";
+
+async function CreaterNewRecharge(rechargeData:CreaterRecharge) {
+    const {phone_id, amount, created_at} = rechargeData;
+    const result = await connection.query<CreaterRecharge>(`
+        INSERT INTO recharge (phone_id, amount, created_at)
+        VALUES ($1, $2, $3)
+        RETURNING *
+        `,[phone_id, amount, created_at])
+        return result.rows[0]
+}
+
+async function findAllRecharge() {
+    const recharges = await connection.query<Recharge>(`SELECT * FROM recharges`)
+    return recharges.rows
+}
+
+async function findRechargeByPhone(phone_number: Number) {
+    const recharge = await connection.query<Recharge>(`
+        SELECT * FROM recharge WHERE phone_number = $1
+        `,[phone_number])
+        return recharge.rows[0]
+}
+
+const rechargeRepository = {
+    CreaterNewRecharge,
+    findAllRecharge,
+    findRechargeByPhone
+}
+
+export default rechargeRepository;
